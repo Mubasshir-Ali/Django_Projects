8e1c78e7c5cf8370fd35d11ec98edf777aa91a58
Mubasshir-ali <mubasshir.ali42@gmail.com>
2021-05-03 02:15:11 +0500

    Create our 1st Django Project

---
+++ Al_ALIENEER/user/routes.py
@@ -0,0 +1,25 @@
+# from flask import Flask
+# from app import app
+# from user.models import User
+
+# @app.route('/user/signup', methods=['GET'])
+# def signup():
+#     return User().signup()
+from flask import Flask
+from app import app
+from user.models import User
+
+
+@app.route('/user/signup', methods=['POST'])
+def signup():
+    return User().signup()
+
+
+@app.route('/user/signout')
+def signout():
+    return User().signout()
+
+
+@app.route('/user/login', methods=['POST'])
+def login():
+    return User().login()
