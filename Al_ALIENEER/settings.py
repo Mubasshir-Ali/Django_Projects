e43e2b57e76312280dd9a03040fbd9a143de15ba
Mubasshir-ali <mubasshir.ali42@gmail.com>
2021-05-03 14:56:20 +0500

    Add settings.py TEMPLATES in 'DIRS': [templates]

--- Al_ALIENEER/settings.py
+++ Al_ALIENEER/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Al_ALIENEER.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [templates],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
