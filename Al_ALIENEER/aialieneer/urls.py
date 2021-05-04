17395099c873144dd8eb3d20aa0ea116618919c6
Mubasshir-ali <mubasshir.ali42@gmail.com>
2021-05-04 14:58:52 +0500

    update views.py, urls.py, aialieneer an setting up index and layout.html

--- Al_ALIENEER/aialieneer/urls.py
+++ Al_ALIENEER/aialieneer/urls.py
@@ -18,6 +18,22 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="AIHome"),
+    path("", views.index, name="AIHome"),
+    path("about/", views.about, name="About"),
+    path("courses/", views.courses, name="Courses"),
+    path("blog/", views.blog, name="Blog"),
+    path("contact/", views.contact, name="Contact"),
+    path("login/", views.login, name="Login"),
+    path("signup/", views.signup, name="Sign"),
+    path("forgot_password/", views.forgot_password, name="Forgot Password"),
+    path("single_post/", views.single_post, name="Single Post"),
+    # Dashboard urls
+    path("dashboard/", views.dashboard, name="Dashboard"),
+    # path("", views.index, name="AIHome"),
+    # path("", views.index, name="AIHome"),
+    # path("", views.index, name="AIHome"),
+    # path("", views.index, name="AIHome"),
+    # path("", views.index, name="AIHome"),
     
+
 ]
--- Al_ALIENEER/aialieneer/views.py
+++ Al_ALIENEER/aialieneer/views.py
@@ -4,3 +4,30 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'aialieneer/index.html')
+
+def about(request):
+    return render(request, 'aialieneer/about.html')
+
+def courses(request):
+    return render(request, 'aialieneer/courses.html')
+
+def blog(request):
+    return render(request, 'aialieneer/blog.html')
+
+def contact(request):
+    return render(request, 'aialieneer/contact.html')
+
+def login(request):
+    return render(request, 'aialieneer/login.html')
+
+def signup(request):
+    return render(request, 'aialieneer/signup.html')
+
+def forgot_password(request):
+    return render(request, 'aialieneer/forgot-password.html')
+
+def single_post(request):
+    return render(request, 'aialieneer/single-post.html')
+
+def dashboard(request):
+    return render(request, 'aialieneer/dashboard.html')
