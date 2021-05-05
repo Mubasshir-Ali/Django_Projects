15ea5003686850338239d55113ff175e745c9fef
Mubasshir-ali <mubasshir.ali42@gmail.com>
2021-05-06 02:50:52 +0500

    Edit settings.py and Models.py

--- Al_ALIENEER/aialieneer/models.py
+++ Al_ALIENEER/aialieneer/models.py
@@ -8,3 +8,7 @@ class Course(models.Model):
     course_author = models.CharField(max_length=100)
     pub_date = models.DateField()
     course_type = models.CharField(max_length=100)
+    image = models.ImageField(upload_to = "aialieneer/images", default = "")
+
+    def __str__(self):
+        return self.course_name
