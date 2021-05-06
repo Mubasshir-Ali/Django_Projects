387d32f3a4bf94106ed223483b2c9a51e2a96f21
Mubasshir-ali <mubasshir.ali42@gmail.com>
2021-05-06 10:57:43 +0500

    update setting.py and urls.py And Add image in Media folder

--- Al_ALIENEER/Al_ALIENEER/urls.py
+++ Al_ALIENEER/Al_ALIENEER/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +25,5 @@ urlpatterns = [
     path('aialieneer', include('aialieneer.urls')),
     path('aialieneer_blog', include('aialieneer_blog.urls')),
     
-]
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
